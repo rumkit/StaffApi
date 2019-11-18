dea0cb6956e4a3b634dfc0bfa4ec7ddfb767c076
Anton Mitsengendler <a.mic@rtc.ru>
2019-11-18 03:38:03 +0300

    Add project files.

---
+++ StaffApi/Data/Migrations/20191118001720_InitialMigration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StaffApi.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    DateOfBirth = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
---
+++ StaffApi/Data/StaffApiContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using StaffApi.Models;
+
+namespace StaffApi.Data
+{
+    public class StaffApiContext : DbContext
+    {
+        public StaffApiContext (DbContextOptions<StaffApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<StaffApi.Models.Employee> Employees { get; set; }
+    }
+}
